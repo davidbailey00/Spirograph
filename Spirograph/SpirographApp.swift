7c62b5eead389d0779f87bee0284ef731c363bc9
David Bailey <davidbailey00@outlook.com>
2021-06-11 16:20:00 +0100

    Initial commit

---
+++ Spirograph/SpirographApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpirographApp.swift
+//  Spirograph
+//
+//  Created by David Bailey on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpirographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
